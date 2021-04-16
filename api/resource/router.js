ef6961530c64ea3d30569ef0dbf2048db5f2599e
alex-pfeifer <apfeif.12@gmail.com>
2021-04-16 01:28:13 -0700

    initial

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,6 @@
-// build your `/api/resources` router here
+const router = require("express").Router();
+
+
+
+
+module.exports = router;
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,8 @@
-// build your `/api/tasks` router here
+const router = require("express").Router();
+
+
+
+
+
+
+module.exports = router;
