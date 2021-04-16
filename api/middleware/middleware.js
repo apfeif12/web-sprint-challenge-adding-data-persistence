7b33b79b8962dbc665785bad4c5d6b99d512b724
alex-pfeifer <apfeif.12@gmail.com>
2021-04-16 15:07:51 -0700

    bugs

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,5 @@
+const Resource = require("../resource/model.js");
+
 function validateProjects(req, res, next) {
     if (req.baseUrl === "/api/projects") {
         if (!req.body.project_name) {
@@ -8,7 +10,22 @@ function validateProjects(req, res, next) {
     } 
 }
 
-
+const checkResourceUnique = async (req, res, next) => {
+    try {
+        const resourceName = await Resource.getById(req.params.id);
+        if (!resourceName) {
+            next();
+        } else {
+            next({
+                status: 400,
+                message: `resource already exists`,
+            });
+        }
+    } catch (error) {
+        next(error);
+    }
+};
 module.exports = {
     validateProjects,
+    checkResourceUnique,
 };
